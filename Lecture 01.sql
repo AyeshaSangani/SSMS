b34d5a2368768f4b7a4789d52fe54b980120872b
Ayesha Sangani <142738168+AyeshaSangani@users.noreply.github.com>
2025-05-04 02:31:51 +0500

    sql commands...

---
+++ Lecture 01.sql
@@ -0,0 +1,17 @@
+1- For Creating Database
+create database "database name"
+
+2- For Creating Table 
+create table "table name" (table-headings);
+
+3- Select Table 
+  select * from "table name";
+
+4- For inserting values in a table
+insert into (id , name , dept) values (101 , '....' , '....' );
+
+5- For Updating the Table
+update "table name"
+set dept='....' where id=101;
+
+6-
