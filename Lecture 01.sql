310e38f77e9c14d38c3c7aa3726933eb0cbecada
Ayesha Sangani <142738168+AyeshaSangani@users.noreply.github.com>
2025-05-07 23:42:59 +0500

    Commands

--- Lecture 01.sql
+++ Lecture 01.sql
@@ -4,7 +4,7 @@ create database "database name"
 2- For Creating Table 
 create table "table name" (table-headings);
 
-3- Select Table 
+3- Selecting Table 
   select * from "table name";
 
 4- For inserting values in a table
@@ -14,4 +14,15 @@ insert into (id , name , dept) values (101 , '....' , '....' );
 update "table name"
 set dept='....' where id=101;
 
-6-
+6-For Deleting 
+  delete student_info where id=3;
+          (table name)
+
+7---truncate 
+truncate table student_info;
+               (table name)  
+
+8- drop table student_info;
+              (table name) 
+                
+9- drop database info;
